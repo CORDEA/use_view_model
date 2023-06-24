46d8786e3caed6fb7fd8a0ed67f206642c415ead
Yoshihiro Tanaka <contact@cordea.jp>
2023-06-24 14:45:36 +0900

    avoid referring to the context on dispose

--- lib/src/use_view_model.dart
+++ lib/src/use_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'view_model.dart';
+import 'view_model_store.dart';
 import 'view_model_store_owner.dart';
 
 T useViewModel<T extends ViewModel>({
@@ -24,22 +25,23 @@ class _ViewModelHookState<T extends ViewModel>
     extends HookState<T, _ViewModelHook<T>> {
   late final viewModel = hook.builder?.call();
   late final isParent = hook.builder != null;
+  late ViewModelStore store;
 
   @override
   build(BuildContext context) {
-    final owner = ViewModelStoreOwner.of(context);
+    store = ViewModelStoreOwner.of(context);
     final viewModel = this.viewModel;
     if (viewModel != null) {
-      owner.put(hook.key, viewModel);
+      store.put(hook.key, viewModel);
       return viewModel;
     }
-    return owner.get(hook.key) as T;
+    return store.get(hook.key) as T;
   }
 
   @override
   void dispose() {
     if (isParent) {
-      ViewModelStoreOwner.of(context).remove(hook.key);
+      store.remove(hook.key);
     }
     super.dispose();
   }
